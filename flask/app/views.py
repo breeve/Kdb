88dee510e9ae5495b6676d31c2001a9ea97d8216
breevewindys <1205034601@qq.com>
2018-11-04 17:43:45 +0800

    no message

--- flask/app/views.py
+++ flask/app/views.py
@@ -80,9 +80,9 @@ def search():
 @app.route("/search_profession")
 def search_profession():
     article_total_nums = 1000
-        return render_template('search_profession.html',
-            title = 'search_profession',
-            total_articles = article_total_nums)    
+    return render_template('search_profession.html',
+        title = 'search_profession',
+        total_articles = article_total_nums)    
 
 @app.route("/personalinfo")
 def personalinfo():
