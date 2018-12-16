2c28c4236064968dbede287b099f48d2d67b58bf
breevewindys <1205034601@qq.com>
2018-12-16 17:14:40 +0800

    no message

--- flask/app/views.py
+++ flask/app/views.py
@@ -246,7 +246,7 @@ def search_key():
     keys_tmp = keys_tmp.split(',')
     keys = []
     for item in keys_tmp :
-        item = s.strip().lstrip().rstrip('\'')
+        item = item.strip().lstrip().rstrip('\'')
         keys.append(item)
 
 
