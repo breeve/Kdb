0ccebbe72f3fc99f1413aff18b3c6552142ca4a9
breevewindys <1205034601@qq.com>
2019-02-25 21:39:03 +0800

    no message

--- flask/app/views.py
+++ flask/app/views.py
@@ -326,7 +326,7 @@ def search_key():
         doc_class=doc_class,
         title = 'Search Key',
         total_articles = 1)
-@app.route("/exit_view"，methods = ["POST"])
+@app.route("/exit_view", methods = ["POST"])
 def exit_view():
     article_total_nums = 1000
     return render_template('exit_view.html',
