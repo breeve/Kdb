7d3b556628e0988faf05353d01527be5e27b636d
breevewindys <1205034601@qq.com>
2019-03-03 13:22:45 +0800

    no message

--- flask/app/views.py
+++ flask/app/views.py
@@ -353,7 +353,7 @@ def exit_view():
         title = 'exit_view',
         total_articles = article_total_nums)
 
-@app.route("/dispatch")
+@app.route("/dispatch", methods="POST")
 def dispatch():
     article_total_nums = 1000
     return render_template('dispatch.html',
