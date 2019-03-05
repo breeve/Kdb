999055c014713061c1ef059221b2dd1e9cf4b611
breeve <1205034601@qq.com>
2019-03-05 09:05:40 -0500

    excel test

---
+++ excel/export_test.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-  
+#! /usr/bin/python
+
+
+import xlrd
+import xlwt
+import openpyxl
+
+def write03Excel(path):
+    wb = xlwt.Workbook()
+    sheet = wb.add_sheet("2003测试表")
+    value = [["名称", "价格", "出版社", "语言"],
+             ["如何高效读懂一本书", "22.3", "机械工业出版社", "中文"],
+             ["暗时间", "32.4", "人民邮电出版社", "中文"],
+             ["拆掉思维里的墙", "26.7", "机械工业出版社", "中文"]]
+    for i in range(0, 4):
+        for j in range(0, len(value[i])):
+            sheet.write(i, j, value[i][j])
+    wb.save(path)
+    print("写入数据成功！")
+
+write03Excel('./2003.xls')
+
+
